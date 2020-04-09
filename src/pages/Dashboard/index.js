c691449c0e974853c97a8b81092f7876910ecd75
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-04-08 21:01:05 -0300

    Added Dashboard style list

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { View } from "react-native";
 
 import Background from "../../components/Background";
 
+import { Container, Title, List } from "./styles";
+import Appointment from "../../components/Appointment";
+
 export default function Dashboard() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+          keyExtractor={(item) => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
