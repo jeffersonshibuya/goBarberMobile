162bfe21ad4576b438a4e5884211282df2f63686
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-04-09 08:29:26 -0300

    Initial - Added appointments list from backend

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Background from "../../components/Background";
 
 import { Container, Title, List } from "./styles";
 import Appointment from "../../components/Appointment";
 
+import api from "../../services/api";
+
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get("appointments");
+
+      setAppointments(response.data);
+    }
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    +console.tron.log(id);
+    const response = await api.delete(`appointment/${id}`);
+
+    setAppointments(
+      appointments.map((appointment) =>
+        appointment.id === id
+          ? { ...appointment, canceled_at: response.data.canceled_at }
+          : appointment
+      )
+    );
+  }
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
 
         <List
-          data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+          data={appointments}
           keyExtractor={(item) => String(item)}
-          renderItem={({ item }) => <Appointment data={item} />}
+          renderItem={({ item }) => (
+            <Appointment data={item} onCancel={() => handleCancel(item.id)} />
+          )}
         />
       </Container>
     </Background>
